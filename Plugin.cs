83d5c9ffea4e4a32a89ebbd495fdc72c1dfc6a59
nikma <nikma@HOME-DESKTOP>
2026-01-31 18:50:17 -0500

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Networking;
+using Console;
+
+// Credits to BrokenStone for the Consoleless Mod. Consoleless Repo --> https://github.com/BrokenSt0ne/Consoleless.
+
+
+namespace ConsolelessConsole
+{
+    [BepInPlugin("icetheperson.ConsolelessConsole", "ConsolelessConsole", "1.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start() => global::Console.Console.LoadConsole();
+
+        public void Awake()
+        {
+            var harmony = new Harmony("icetheperson.ConsolelessConsole");
+            harmony.PatchAll();
+            Debug.Log("[ConsolelessConsole] Initialized");
+        }
+    }
+
+    public class Constants
+    {
+        public static List<string> BlockedUrls = new List<string>()
+        {
+            "https://iidk.online/",
+            "https://raw.githubusercontent.com/iiDk-the-actual/Console",
+            "https://data.hamburbur.org",
+            "https://files.hamburbur.org"
+        };
+    }
+
+    [HarmonyPatch(typeof(UnityWebRequest), nameof(UnityWebRequest.SendWebRequest))]
+    public class UnityWebRequestPatch
+    {
+        [HarmonyPrefix]
+        static bool Prefix(UnityWebRequest __instance)
+        {
+            if (Constants.BlockedUrls.Any(blocked => __instance.url.StartsWith(blocked)))
+            {
+                Debug.Log($"[ConsolelessConsole] Blocked {__instance.url}");
+                __instance.url = null;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(HttpClient), nameof(HttpClient.GetByteArrayAsync), new[] { typeof(string) })]
+    public class HttpClientPatch
+    {
+        [HarmonyPrefix]
+        static bool Prefix(string requestUri, ref Task<byte[]> __result)
+        {
+            if (Constants.BlockedUrls.Any(blocked => requestUri.StartsWith(blocked)))
+            {
+                Debug.Log($"[ConsolelessConsole] Blocked {requestUri}");
+                __result = Task.FromResult(new byte[0]);
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,26 @@
+# ConsolelessConsole
+Credits to BrokenSt0ne for the Original Mod. Consoleless Repo --> https://github.com/BrokenSt0ne/Consoleless
+
+A BepInEx mod that prevents [Console](https://github.com/iiDk-the-actual/Console) from sending telemetry data and accessing the internet (mostly) while using Console.
+
+This currently blocks a majority of endpoints on iidk.online, but not the friend system.
+
+and yes this code is messy please fix it thank you
+
+# Info
+II's menu sends telemetry data every time you join a room, this includes your:
+- Room name
+- Nickname
+- Player ID
+- Room visibility
+- Room's playerlist
+- Room's player count
+- Gamemode
+Also:
+This mod contains the console that sends telemetry data every time you join a room.
+
+
+There is no way to disable this as far as I'm aware in his menu. This is the backbone of his tracker afaik.
+
+# WARNING
+Due to the nature of this mod, it ***WILL*** break some **(if not most)** features of mods that include console or that need to contact iidk's server data or the hamburbur servers. I am not responsible for any incidents that occur because of this. Also this does include console so beware.
